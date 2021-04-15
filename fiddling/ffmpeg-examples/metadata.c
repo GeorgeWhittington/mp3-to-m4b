54d0d6622ec036d8316cf2a19f1e9334185125c7
George Whittington <80219357+GeorgeWhittington@users.noreply.github.com>
2021-04-15 20:36:48 +0100

    Made progress on writing transcoding and remuxing code in cpp, pretty sure I've figured out chapter metadata.

--- fiddling/ffmpeg-examples/metadata.c
+++ fiddling/ffmpeg-examples/metadata.c
@@ -31,6 +31,43 @@
 #include <libavformat/avformat.h>
 #include <libavutil/dict.h>
 
+size_t av_strlcpy(char *dst, const char *src, size_t size) {
+    size_t len = 0;
+    while (++len < size && *src)
+        *dst++ = *src++;
+    if (len <= size)
+        *dst = 0;
+    return len + strlen(src) - 1;
+}
+
+void dump_metadata_mine(void *ctx, const AVDictionary *m, const char *indent) {
+    if (m && !(av_dict_count(m) == 1 && av_dict_get(m, "language", NULL, 0))) {
+        const AVDictionaryEntry *tag = NULL;
+
+        av_log(ctx, AV_LOG_INFO, "%sMetadata:\n", indent);
+        while ((tag = av_dict_get(m, "", tag, AV_DICT_IGNORE_SUFFIX)))
+            if (strcmp("language", tag->key)) {
+                const char *p = tag->value;
+                av_log(ctx, AV_LOG_INFO,
+                       "%s  %-16s: ", indent, tag->key);
+                while (*p) {
+                    char tmp[256];
+                    size_t len = strcspn(p, "\x8\xa\xb\xc\xd");
+                    av_strlcpy(tmp, p, FFMIN(sizeof(tmp), len + 1));
+                    av_log(ctx, AV_LOG_INFO, "%s", tmp);
+                    p += len;
+                    if (*p == 0xd)
+                        av_log(ctx, AV_LOG_INFO, " ");
+                    if (*p == 0xa)
+                        av_log(ctx, AV_LOG_INFO, "\n%s  %-16s: ", indent, "");
+                    if (*p)
+                        p++;
+                }
+                av_log(ctx, AV_LOG_INFO, "\n");
+            }
+    }
+}
+
 int main (int argc, char **argv)
 {
     AVFormatContext *fmt_ctx = NULL;
@@ -51,6 +88,44 @@ int main (int argc, char **argv)
         av_log(NULL, AV_LOG_ERROR, "Cannot find stream information\n");
         return ret;
     }
+
+    AVStream *audio_stream;
+    int audio_stream_ind = -1;
+
+    for (unsigned int i = 0; i < fmt_ctx->nb_streams; i++) {
+        AVStream *stream = fmt_ctx->streams[i];
+        if (stream->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
+            audio_stream = stream;
+            audio_stream_ind = i;
+            break;
+        }
+    }
+
+    if (audio_stream_ind >= 0) {
+        AVCodec *codec = avcodec_find_decoder(audio_stream->codecpar->codec_id);
+        AVCodecContext *codec_ctx;
+        AVFrame *frame;
+        if (!codec) {
+            return AVERROR_DECODER_NOT_FOUND;
+        }
+        codec_ctx = avcodec_alloc_context3(codec);
+        if (!codec_ctx) {
+            return AVERROR(ENOMEM);
+        }
+        ret = avcodec_parameters_to_context(codec_ctx, audio_stream->codecpar);
+        if (ret < 0) {
+            return ret;
+        }
+        ret = avcodec_open2(codec_ctx, codec, NULL);
+        if (ret < 0) {
+            return ret;
+        }
+
+    }
+    
+    av_dump_format(fmt_ctx, 0, argv[1], 0);
+
+    dump_metadata_mine(NULL, fmt_ctx->metadata, "  ");
 
     while ((tag = av_dict_get(fmt_ctx->metadata, "", tag, AV_DICT_IGNORE_SUFFIX)))
         printf("%s=%s\n", tag->key, tag->value);
--- fiddling/ffmpeg-examples/transcoding.cc
+++ fiddling/ffmpeg-examples/transcoding.cc
@@ -4,7 +4,20 @@
 
 #include <iostream>
 
-// Code to transcode to aac and remux to m4b (mpeg4)
+// Code to transcode audio files to aac and remux to m4b (mpeg4)
+
+class TranscodeContext {
+  public:
+    int open_input_file(std::string filename);
+    int open_output_file(std::string filename);
+
+  protected:
+    av::FormatContext input_format_ctx;
+    av::FormatContext output_format_ctx;
+
+    av::AudioDecoderContext decode_ctx;
+    av::AudioEncoderContext encode_ctx;
+};
 
 bool has_ending(std::string const &full_string, std::string const &ending) {
   if (full_string.length() >= ending.length()) {
@@ -14,9 +27,10 @@ bool has_ending(std::string const &full_string, std::string const &ending) {
   }
 }
 
-int open_input_file(std::string filename) {
+// Open the input file, find decoder for the audio stream,
+// open the decoder
+int TranscodeContext::open_input_file(std::string filename) {
   std::error_code err;
-  av::FormatContext input_format_ctx;
 
   input_format_ctx.openInput(filename, err);
   if (err) {
@@ -48,7 +62,6 @@ int open_input_file(std::string filename) {
     return -1;
   }
 
-  av::AudioDecoderContext decode_ctx;
   decode_ctx = av::AudioDecoderContext(audio_stream);
   av::Codec codec = av::findDecodingCodec(decode_ctx.raw()->codec_id);
   decode_ctx.open(av::Codec(), err);
@@ -60,8 +73,104 @@ int open_input_file(std::string filename) {
   // Create a frame? dunno.
 }
 
-int open_output_file(std::string filename) {
+// Prepare the encoder context and the stream that will be written to.
+// Open the output file, add chapter metadata, write the header to the
+// output file
+int TranscodeContext::open_output_file(std::string filename) {
+  // To add chapter data! See here: https://www.ffmpeg.org/doxygen/trunk/structAVChapter.html
+  // Probably just need to create chapters, add them to output_format_ctx
+  // and they'll write automatically!
 
+  // General code to add chapters:
+
+  // av::Dictionary metadata;
+  // metadata.set("title", "Chapter One");
+  // AVChapter *chapter;
+  // chapter->id = 0;
+  // chapter->time_base = av::Rational(1000, 1);
+  // chapter->start = 0;
+  // chapter->end = 10000; // 10s in? I think.
+  // chapter->metadata = metadata.raw();
+  // AVChapter **chapters;
+  // chapters[0] = chapter;
+  // output_format_ctx.raw()->chapters = chapters;
+  // output_format_ctx.raw()->nb_chapters = 1;
+
+  // Now that I know how to add chapters, need to decide when exact duration data
+  // will be collected. One pass decoding full file to null buffer def needed
+  // to get the exact length of each file, but will have to decide if that's done
+  // on original load to table, or in a separate step just after convert button
+  // pressed, before actual conversion.
+
+  std::error_code err;
+
+  av::guessOutputFormat("", "m4b", "");
+
+  av::Codec output_codec = av::findEncodingCodec(AV_CODEC_ID_AAC);
+  if (output_codec.canEncode()) {
+    // Will have to see if this is enough of a check?
+    std::cerr << "Encoder fetched cannot encode" << std::endl;
+    return -1;
+  }
+
+  auto out_stream = output_format_ctx.addStream(output_codec, err);
+  if (err) {
+    std::cerr << "Couldn't add a stream to the output context" << std::endl;
+    return -1;
+  }
+
+  encode_ctx = av::AudioEncoderContext(output_codec);
+
+  encode_ctx.setChannels(2);
+  encode_ctx.setChannelLayout(av_get_default_channel_layout(2));
+  // cloning sample stuff, once there's multiple inputs will need to
+  // convert inputs to a single output sample rate, maybe reference:
+  // https://github.com/h4tr3d/avcpp/blob/master/example/api2-samples/api2-decode-rasample-audio.cpp
+  // for that.
+  encode_ctx.setSampleRate(decode_ctx.sampleRate());
+  encode_ctx.setSampleFormat(output_codec.supportedSampleFormats()[0]);
+  encode_ctx.setBitRate(96000);
+
+  encode_ctx.setStrict(FF_COMPLIANCE_EXPERIMENTAL);
+
+  encode_ctx.setTimeBase(av::Rational(decode_ctx.sampleRate(), 1));
+
+  if (output_format_ctx.outputFormat().flags() & AVFMT_GLOBALHEADER) {
+    encode_ctx.setFlags2(encode_ctx.flags() || AV_CODEC_FLAG_GLOBAL_HEADER);
+  }
+
+  encode_ctx.open(err);
+  if (err) {
+    std::cerr << "Can't open codec" << std::endl;
+    return -1;
+  }
+
+  int status = avcodec_parameters_from_context(out_stream.raw()->codecpar, encode_ctx.raw());
+  if (status < 0) {
+    std::cerr << "Failed to copy encoder parameters to output stream" << std::endl;
+    return status;
+  }
+
+  out_stream.setTimeBase(encode_ctx.timeBase());
+
+  // log
+  av_dump_format(output_format_ctx.raw(), 0, filename.c_str(), 1);
+
+  // open output file itself
+  output_format_ctx.openOutput(filename, err);
+  if (err) {
+    std::cerr << "Could not open output file: " << filename << std::endl;
+    return -1;
+  }
+
+  // Need to add chapter data to context HERE
+
+  // Init muxer by writing header to output
+  output_format_ctx.writeHeader(err);
+  if (err) {
+    std::cerr << "Error occured writing header to output file" << std::endl;
+    return -1;
+  }
 }
 
 int main(int argc, char **argv) {
@@ -81,14 +190,14 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  // Probably instantiate objects here and pass them in?
-  // (But ovs refactor that in later, for now just make them inside)
-  int status = open_input_file(input_filename);
+  TranscodeContext transcode_ctx;
+
+  int status = transcode_ctx.open_input_file(input_filename);
   if (status < 0) {
     return 1;
   }
 
-  status = open_output_file(output_filename);
+  status = transcode_ctx.open_output_file(output_filename);
   if (status < 0) {
     return 1;
   }
